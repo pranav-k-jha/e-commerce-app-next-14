bbf7a41d2fbf5697509f2fae1e9e00979f7228e5
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-05 17:21:33 -0400

    added functionalities to drop down navbar items

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,8 +1,35 @@
 "use client";
 
+import { PRODUCT_CATEGORIES } from "@/config";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 export default function NavItems() {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
-  return <div className="flex gap-4 h-full"></div>;
+  const isAnyOpen = activeIndex !== null;
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 }
